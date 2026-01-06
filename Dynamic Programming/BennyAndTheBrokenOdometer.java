1fa96ba7a0e7cd454741dde2446a38e166ef70af
Priyanshu-Jangra-0007 <priyanshujangra145@gmail.com>
2026-01-06 10:29:32 +0530

    BennyAndTheBrokenOdometer

--- Dynamic Programming/BennyAndTheBrokenOdometer.java
+++ Dynamic Programming/BennyAndTheBrokenOdometer.java
@@ -1,10 +1,34 @@
+import java.util.*;
+
 public class BennyAndTheBrokenOdometer {
 
     public static long getRealDistance(long displayedValue) {
-        return 0;
+        long realDistance = 0;
+        long base = 1;
+
+        while (displayedValue > 0) {
+            long digit = displayedValue % 10;
+            if (digit > 3) {
+                digit--;
+            }
+
+            realDistance += digit * base;
+            base *= 9;
+            displayedValue /= 10;
+        }
+
+        return realDistance;
     }
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            long N = sc.nextLong();
+            System.out.println(getRealDistance(N));
+        }
+
+        sc.close();
     }
 }
