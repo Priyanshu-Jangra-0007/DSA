ef1d14c141dc1638d124e469b9bb69fb4efc8b2c
Priyanshu Jangra <priyanshujangra145@gmail.com>
2026-02-09 11:56:46 +0530

    Labyrinth

--- Graphs/Labyrinth.java
+++ Graphs/Labyrinth.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -11,21 +9,20 @@ import java.util.StringTokenizer;
 
 public class Labyrinth {
 
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
+    static int dr[] = {-1, 1, 0, 0};
+    static int dc[] = {0, 0, -1, 1};
     static char[] dir = {'U', 'D', 'L', 'R'};
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        boolean[][] visited = new boolean[n][m];
-        char[][] parent = new char[n][m];
+        boolean visited[][] = new boolean[n][m];
+        char parent[][] = new char[n][m];
         char[][] grid = new char[n][m];
 
         int sr = 0, sc = 0, er = 0, ec = 0;
@@ -34,8 +31,14 @@ public class Labyrinth {
             String s = br.readLine();
             for (int j = 0; j < m; j++) {
                 grid[i][j] = s.charAt(j);
-                if (grid[i][j] == 'A') { sr = i; sc = j; }
-                if (grid[i][j] == 'B') { er = i; ec = j; }
+                if (grid[i][j] == 'A') {
+                    sr = i;
+                    sc = j;
+                }
+                if (grid[i][j] == 'B') {
+                    er = i;
+                    ec = j;
+                }
             }
         }
 
@@ -44,28 +47,24 @@ public class Labyrinth {
         visited[sr][sc] = true;
 
         while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int r = cur[0], c = cur[1];
+            int[] curr = q.poll();
+            int r = curr[0];
+            int c = curr[1];
+
+            if (r == er && c == ec) break;
 
             for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
+                int nR = r + dr[i];
+                int nC = c + dc[i];
 
-                if (nr >= 0 && nr < n && nc >= 0 && nc < m
-                        && !visited[nr][nc]
-                        && (grid[nr][nc] == '.' || grid[nr][nc] == 'B')) {
+                if (nR >= 0 && nR < n && nC >= 0 && nC < m && !visited[nR][nC]
+                        && (grid[nR][nC] == '.' || grid[nR][nC] == 'B')) {
 
-                    visited[nr][nc] = true;
-                    parent[nr][nc] = dir[i];
-                    q.offer(new int[]{nr, nc});
+                    visited[nR][nC] = true;
+                    parent[nR][nC] = dir[i];
+                    q.offer(new int[]{nR, nC});
                 }
             }
-        }
-
-        if (!visited[er][ec]) {
-            bw.write("NO\n");
-            bw.flush();
-            return;
         }
 
         ArrayList<Character> path = new ArrayList<>();
@@ -74,6 +73,7 @@ public class Labyrinth {
         while (i != sr || j != sc) {
             char p = parent[i][j];
             path.add(p);
+
             if (p == 'U') i++;
             else if (p == 'D') i--;
             else if (p == 'L') j++;
@@ -82,11 +82,11 @@ public class Labyrinth {
 
         Collections.reverse(path);
 
-        bw.write("YES\n");
-        bw.write(path.size() + "\n");
-        for (char c : path) bw.write(c);
-        bw.write("\n");
-
-        bw.flush();
+        System.out.println("YES");
+        System.out.println(path.size());
+        for (char c : path) {
+            System.out.print(c);
+        }
+        System.out.println();
     }
 }
