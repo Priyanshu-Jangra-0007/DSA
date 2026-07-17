cec3586a46ff67c4ec441651ddc7e27a1d1dfd2d
Priyanshu . <priyanshujangra145@gmail.com>
2026-07-17 14:22:44 +0530

    Time: 1 ms (10.28%), Space: 48.1 MB (47.83%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        return solve(x,N);
+    }
+    public double solve(double x,long N){
+        if(N==0) return 1;
+        if(N%2==0){
+            return solve(x*x,N/2);
+        }
+        else{
+            return x*solve(x,N-1);
+        }
+    }
+}
