8135150eb512298539f1517a684a707134bfe915
Priyanshu . <priyanshujangra145@gmail.com>
2026-07-21 14:36:41 +0530

    Time: 2 ms (99.2%), Space: 119.7 MB (9.97%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -7,8 +7,8 @@ class Solution {
             totalcost+=cost[i];
         }
         if(totalcost>totalgas) return -1;
-        int st=0;
         int tank=0;
+        int st=0;
         for(int i=0;i<gas.length;i++){
             tank+=gas[i]-cost[i];
             if(tank<0){
