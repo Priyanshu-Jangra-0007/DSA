451cdf5ee612ad3566907dcf497510dbe77fff44
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-15 13:43:43 +0530

    Time: 1 ms (100%), Space: 72.1 MB (26.36%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cs
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cs
@@ -1,11 +1,18 @@
 public class Solution {
     public int FindDuplicate(int[] nums) {
-        Array.Sort(nums);
-        for(int i=0;i<nums.Length-1;i++){
-            if(nums[i]==nums[i+1]){
-                return nums[i];
-            }
+        int n = nums.Length;
+        var freq = new int[n + 1];
+        int result = 0;
+        for(int i = 0; i < nums.Length; i++){
+            freq[nums[i]]++;
         }
-        return -1;
+        for(int i = 0; i < freq.Length; i++){
+            if(freq[i] > 1){
+                result = i;
+                break;
+            }           
+        }
+
+        return result;
     }
 }
