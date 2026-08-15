00a5db0d6cc804ba8bda7f8f7486ffd5366c3c0e
Priyanshu . <priyanshujangra145@gmail.com>
2026-08-15 09:45:16 +0530

    Time: 1 ms (99.54%), Space: 49.3 MB (62.1%) - LeetHub

--- 0001-two-sum/0001-two-sum.cs
+++ 0001-two-sum/0001-two-sum.cs
@@ -1,12 +1,12 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        int sum=0;
-        for(int i=0;i<nums.Length;i++){
-            for(int j=i+1;j<nums.Length;j++){
-                sum+=nums[i]+nums[j];
-                if(sum==target) return new int[]{i,j};
-                else sum=0;
+        Dictionary<int,int> map=new Dictionary<int,int>();
+        map.TryAdd(nums[0],0);
+        for(int i=1;i<nums.Length;i++){
+            if(map.TryGetValue(target-nums[i],out int index)){
+                return new int[]{i,index};
             }
+            map.TryAdd(nums[i],i);
         }
         return new int[]{-1,-1};
     }
