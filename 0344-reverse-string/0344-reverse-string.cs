1ff51dd221fde00b10467c9d06d960ac82ca2bd6
Priyanshu . <priyanshujangra145@gmail.com>
2026-09-01 18:17:49 +0530

    Time: 0 ms (100%), Space: 64.9 MB (58.41%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cs
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -3,9 +3,7 @@ public class Solution {
         int i=0;
         int j=s.Length-1;
         while(i<j){
-            char temp=s[i];
-            s[i]=s[j];
-            s[j]=temp;
+            (s[i],s[j])=(s[j],s[i]);
             i++;
             j--;
         }
