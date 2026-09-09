8446a66eaba740ad86561bc5925eceae3e7735b5
Priyanshu . <priyanshujangra145@gmail.com>
2026-09-09 15:44:45 +0530

    Time: 10 ms (27.74%), Space: 47.3 MB (17.69%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,22 +1,25 @@
 class Solution {
+    static Integer dp[];
     public int deleteAndEarn(int[] nums) {
-        int max=0;
-        int n=nums.length;
-        for(int x:nums){
-            max=Math.max(max,x);
+        Arrays.sort(nums);
+        int max=-(int) 1e9;
+        for(int i=0;i<nums.length;i++){
+            max=Math.max(nums[i],max);
         }
-        int arr[]=new int[max+1];
-        for(int i=0;i<n;i++){
+        int n=max+1;
+        dp=new Integer[n+1];
+        int arr[]=new int[n];
+        for(int i=0;i<nums.length;i++){
             arr[nums[i]]+=nums[i];
         }
-        Integer dp[]=new Integer[max+1];
-        return solve(arr,max,dp);
+        return solve(arr,n-1);
     }
-    public static int solve(int arr[],int n,Integer dp[]){
+    public static int solve(int arr[],int n){
         if(n<0) return 0;
+        if(n==0) return arr[0];
         if(dp[n]!=null) return dp[n];
-        int pick=solve(arr,n-2,dp)+arr[n];
-        int skip=solve(arr,n-1,dp);
-        return dp[n]=Math.max(pick,skip);
+        int left=solve(arr,n-2)+arr[n];
+        int right=solve(arr,n-1);
+        return dp[n]=Math.max(left,right);
     }
 }
